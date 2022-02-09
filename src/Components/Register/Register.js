a01bdba530211a9bcb3abfd508e77fe3ca4e14df
Ashraful <ashrafulislam2880@gmail.com>
2022-02-09 16:56:34 +0600

    login Page Added

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,74 +1,82 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from '../../Components/Images/Favicon.png';
+import logo2 from '../../Components/Images/Main-Logo.png';
+
 const Register = () => {
   return (
     <div className="bg-gray-400">
-      <div className="w-full max-w-xs mx-auto pt-8 pb-24">
-        <h1 className="text-2xl font-bold uppercase mb-4 text-yellow-300">
-          Please Register To<br /> Yodda Hostel
-        </h1>
-        
-        <form className="bg-white shadow-md rounded p-10">
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-left text-sm font-bold mb-2"
-              for="fullname"
-            >
-              Name
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="fullname"
-              type="text"
-              placeholder="Full Name"
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-left text-sm font-bold mb-2"
-              for="email"
-            >
-              Email
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="email"
-              type="email"
-              placeholder="Email"
-            />
-          </div>
-          <div className="mb-2">
-            <label
-              className="block text-gray-700 text-left text-sm font-bold mb-2"
-              for="password"
-            >
-              Password
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-2 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-              type="password"
-              placeholder="******************"
-            />
-          </div>
-          <div className="flex">
-            <p className="mb-3 text-left">Aready have an account ?</p>
-            &nbsp;{" "}
-            <Link to="/login" className="text-orange-500">
-              Sign In
-            </Link>
-          </div>
-          <div className="flex items-center justify-between">
-            <button
-              className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
-              type="button"
-            >
-              Sign Up
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+            <div className="xl:px-20 md:px-10 sm:px-6 px-4 md:py-12 py-9 2xl:mx-auto 2xl:container md:flex items-center justify-center">
+                <div className=" md:hidden sm:mb-8 mb-6">
+                   <img src={logo} alt="logo"/>
+                </div>
+                <div className="bg-white shadow-lg rounded xl:w-1/3 lg:w-5/12 md:w-1/2 w-full lg:px-10 sm:px-6 sm:py-10 px-2 py-6">
+                    <p className="focus:outline-none text-2xl font-extrabold leading-6 text-gray-800 mb-4">Please Register in</p>
+                    <p className="focus:outline-none text-xl font-extrabold leading-6 text-gray-700 mb-6">Yodda Hostel</p>
+                    <img className="w-32 rounded-3xl mx-auto mb-6" src={logo} alt="logo"/> 
+                    <form onSubmit="handleRegister">
+                        {/* {user.email?setError(''): <span className="text-red-600">{error}</span> } */}
+                        <div>
+                            <label 
+                                htmlFor="email" 
+                                className="text-sm font-medium leading-none text-gray-800"> 
+                                    Email 
+                            </label>
+                            <input 
+                                type="email" 
+                                className="bg-gray-200 border rounded text-xs font-medium leading-none placeholder-gray-500 text-gray-800 py-3 w-full pl-3 mt-2 focus:outline-none focus:ring-2 focus:ring-indigo-400" 
+                                placeholder="example@gmail.com "
+                                onBlur="{handleEmail}" />
+                        </div>
+                        <div className="mt-6 w-full">
+                            <label htmlFor="myInput" className="text-sm font-medium leading-none text-gray-800"> Password </label>
+                            <div className="relative flex items-center justify-center">
+                                <input
+                                    type="password" 
+                                    className="bg-gray-200 border rounded text-xs font-medium leading-none text-gray-800 py-3 w-full pl-3 mt-2 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-400" 
+                                    placeholder="Your Password"
+                                    onBlur="{handlePassword}"
+                                />
+                            </div>
+                        </div>
+                        <div className="mt-8">
+                            <button 
+                                type="submit" 
+                                className="focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 text-sm font-semibold leading-none text-white focus:outline-none bg-indigo-700 border rounded-xl hover:bg-indigo-600 py-4 w-full">
+                                    Register
+                            </button>
+                        </div>
+                    </form>
+                    <p className="focus:outline-none text-sm mt-4 font-medium leading-none text-gray-500">Allready Registerd? 
+                        <Link 
+                            to="/login" 
+                            className="ml-2 text-red-500 focus:text-gray-500 focus:outline-none focus:underline hover:underline text-sm font-medium leading-none cursor-pointer">
+                            Login here
+                        </Link>
+                    </p>
+                </div>
+                <div className="xl:w-1/3 md:w-1/2 lg:ml-16 ml-8 md:mt-0 mt-6">
+                    <div className="pl-8 md:block hidden">
+                        <img src={logo2} alt="logo"/> 
+                    </div>
+                    <div className="flex items-start mt-8">
+                        <div>
+                           <i className="fas fa-quote-left"></i>
+                        </div>
+                        <p className="sm:text-2xl text-xl leading-7 text-gray-600 pl-2.5 text-left ml-4">Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what topic the random paragraph will be about when it appears</p>
+                    </div>
+                    <div className="flex items-center pl-8 mt-10">
+                        <div className="w-8 h-8">
+                            <img src="https://avatars.githubusercontent.com/u/86690202?v=4" alt="profile" className="w-full h-full rounded-2xl" />
+                        </div>
+                        <div className="ml-2">
+                            <p className="text-sm font-medium leading-none text-gray-800">Ashraful Islam</p>
+                            <p className="text-sm font-medium leading-none text-gray-600 mt-1 cursor-pointer hover:underline">See profile</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
   );
 };
 
