642520638ef7330436c567fc1fe087a1e0bb3b19
Ashraful <ashrafulislam2880@gmail.com>
2022-02-10 14:56:39 +0600

    Manage Students Page Developeed

--- src/Components/Dashboard/ManageStudents/ManageStudents.js
+++ src/Components/Dashboard/ManageStudents/ManageStudents.js
@@ -1,9 +1,121 @@
 import React from 'react';
+import useStudents from '../../../Hooks/UseStudents';
 
 const ManageStudents = () => {
+    const [students]=useStudents();
+     //<-------------- Food Delete Function ------------>
     return (
-        <div>
-            Manage Students
+        <div className="container mx-auto flex flex-col">
+          <div className="w-full my-5">
+              <div className="my-2 p-1 bg-white flex border border-gray-200 rounded">
+                  <div className="flex flex-auto flex-wrap"></div>
+                    <input 
+                      placeholder="Search by Roll" 
+                      className="p-1 px-2 appearance-none outline-none w-full text-gray-800"/>
+                    <div className="text-gray-300 w-8 py-1 pl-2 pr-1 border-l flex items-center border-gray-200">
+                        <button className="cursor-pointer w-6 h-6 text-gray-600 outline-none focus:outline-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" className="feather feather-chevron-up w-4 h-4">
+                                <polyline points="18 15 12 9 6 15"></polyline>
+                            </svg>
+                        </button>
+                    </div>
+                  </div>
+              </div>
+          <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+              <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-200">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Roll
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Name
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Age
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Class
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Hall Name
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Status
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Check ALl
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Actions
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200">
+                    {students.map((student) => (
+                      <tr key={student._id}>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="flex items-center">
+                              <div className="text-sm font-medium text-gray-900">{student.roll}</div>
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="flex items-center">
+                              <div className="text-sm font-medium text-gray-900">{student.name}</div>
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="flex items-center">
+                              <div className="text-sm font-medium text-gray-900">{student.age}</div>
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="flex items-center">
+                              <div className="text-sm font-medium text-gray-900">{student.class}</div>
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="flex items-center">
+                              <div className="text-sm font-medium text-gray-900">{student.hall_name}</div>
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="flex items-center">
+                          {
+                            student.status==="Active"?
+                              <div className="text-sm font-medium text-gray-900 bg-green-200 px-2 rounded-lg">{student.status}</div>:
+                              <div className="text-sm font-medium text-gray-900 bg-red-300 px-2 rounded-lg">{student.status}</div>
+                          }
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="flex items-center">
+                              <div className="text-sm font-medium text-gray-900 ml-6">
+                                <input type="checkbox" className="checkbox checkbox-primary"/>
+                              </div>
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="flex items-center">
+                              <div className="text-sm font-medium text-gray-900">
+                                <select id="stDetails" name="carlist" form="carform">
+                                    <option value="select">Select Actions</option>
+                                    <option value="delete">Delete</option>
+                                    <option value="edit">Edit</option>
+                                    <option value="status">Change Status</option>
+                                </select>
+                              </div>
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
     );
 };
--- src/Components/Students/Students.js
+++ src/Components/Students/Students.js
@@ -13,7 +13,7 @@ const Students = () => {
                       className="p-1 px-2 appearance-none outline-none w-full text-gray-800"/>
                     <div className="text-gray-300 w-8 py-1 pl-2 pr-1 border-l flex items-center border-gray-200">
                         <button className="cursor-pointer w-6 h-6 text-gray-600 outline-none focus:outline-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-chevron-up w-4 h-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-chevron-up w-4 h-4">
                                 <polyline points="18 15 12 9 6 15"></polyline>
                             </svg>
                         </button>
