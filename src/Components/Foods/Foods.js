29dd6301c3236d55153a9e88baf455c4b1f0b962
Md Ashraful Islam <ashrafulislam2880@gmail.com>
2023-02-10 01:00:00 +0600

    old Api Issue Resolved

--- src/Components/Foods/Foods.js
+++ src/Components/Foods/Foods.js
@@ -5,13 +5,13 @@ const Foods = () => {
     const [foods] = useFoods();
     return (
         <div className="bg min-h-screen">
-            <h2 className="pt-6 font-bold text-3xl text-white">Our Food Items</h2>
+            <h2 className="pt-10 font-bold text-3xl text-white">Our Food Items</h2>
             {foods?.length > 0 ?
                 <div className="mx-auto container py-8">
-                    <div className="flex flex-wrap items-center lg:justify-between justify-center">
+                    <div className="grid lg:grid-cols-5 md:grid-cols-3 grid-cols-2">
                         {
                             foods.map(food =>
-                                <div className="mx-2 w-72 lg:mb-6 " key={food._id}>
+                                <div className="mx-2 w-72 lg:mb-6 cursor-pointer hover:-translate-y-2 duration-300" key={food?._id}>
                                     <div>
                                         <img src={food?.image} className="w-full h-44 hover:-translate-y-2 transition-all ease-linear duration-200" alt="food" />
                                     </div>
@@ -23,6 +23,9 @@ const Foods = () => {
                                             </div>
                                             <p className="text-xs text-gray-600 mt-2 text-left">The food system has significant impacts on a wide range of other social and political issues including</p>
                                         </div>
+                                        <button onClick={() => alert("Food Added")} className="px-4 py-1.5 w-40 mb-4 border-2 border-orange-500 rounded-md bg-orange-500 text-white hover:text-orange-600 hover:bg-transparent duration-300">
+                                            Add
+                                        </button>
                                     </div>
                                 </div>
                             )
--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Foods from '../../Foods/Foods';
-import Students from '../../Students/Students';
 import Banner from '../Banner/Banner';
+import About from './../../About/About';
+import Contact from './../../Contact/Contact';
 
 const Home = () => {
     return (
         <>
-            <Banner/>
-            <Foods/>
-            <Students/>
+            <Banner />
+            <Foods />
+            <About />
+            <Contact />
         </>
     );
 };
